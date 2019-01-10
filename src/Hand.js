e53481bf4e63f927f1d3b67294d8e1f325b8972d
Brayn Brigoli <32309779+ilbrigz@users.noreply.github.com>
2019-01-10 13:00:52 +0800

    mininute hand ok

--- src/Hand.js
+++ src/Hand.js
@@ -11,48 +11,59 @@ const StyledHand = styled.div`
   left: ${props => (50 - parseInt(props.length, 10)).toString() + "%" || "50%"};
   transform-origin: 100%;
   transform: rotate(90deg);
-  transition: all 0.5s;
-  transition-timing-function: cubic-bezier(0.1, 2.7, 0.58, 1);
 `;
 class Hand extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { intervalId: "" };
     this.getSeconds = this.getSeconds.bind(this);
     this.getMinutes = this.getMinutes.bind(this);
     this.getHours = this.getHours.bind(this);
     this.setHandPosition = this.setHandPosition.bind(this);
   }
   componentDidMount() {
-    setInterval(this.setHandPosition, 1000);
     this.setHandPosition();
-  }
-  setHandPosition() {
-    const now = new Date();
-    if (this.props.second) {
-      this.getSeconds(now);
-    } else if (this.props.minute) {
-      this.getMinutes(now);
-    } else {
-      this.getHours(now);
-    }
-  }
-  getSeconds(t) {
-    const hand = ReactDOM.findDOMNode(this);
-    const seconds = t.getSeconds();
-    const secondsDegrees = (seconds / 60) * 360 + 90;
-    hand.style.transform = `rotate(${secondsDegrees}deg)`;
+    var intervalId = setInterval(this.setHandPosition, 1000);
+    this.setState({ handPosition: intervalId });
   }
 
-  getMinutes(t) {
+  componentWillUnmount = () => {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.handPosition);
+  };
+
+  setHandPosition() {
     const hand = ReactDOM.findDOMNode(this);
+    const now = new Date();
+    if (this.props.second) {
+      this.getSeconds(now, hand);
+    } else if (this.props.minute) {
+      this.getMinutes(now, hand);
+    } else {
+      this.getHours(now, hand);
+    }
+  }
+  getSeconds(t, hand) {
+    const seconds = t.getSeconds();
+    let secondsDegrees = (seconds / 60) * 360 + 90;
+    hand.style.transition = "all 0.5s";
+    hand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if (seconds === 59) {
+      secondsDegrees = (seconds / 60) * 360 + 90 - 360;
+      setTimeout(() => {
+        hand.style.transition = "none";
+        hand.style.transform = `rotate(${secondsDegrees}deg)`;
+      }, 500);
+    }
+  }
+
+  getMinutes(t, hand) {
     const mins = t.getMinutes();
     const minsDegrees = (mins / 60) * 360 + (t.getSeconds() / 60) * 6 + 90;
     hand.style.transform = `rotate(${minsDegrees}deg)`;
   }
 
-  getHours(t) {
-    const hand = ReactDOM.findDOMNode(this);
+  getHours(t, hand) {
     const hour = t.getHours();
     const hourDegrees = (hour / 12) * 360 + (t.getMinutes() / 60) * 30 + 90;
     hand.style.transform = `rotate(${hourDegrees}deg)`;
